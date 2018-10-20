cd84e759e4325e74ee6db6dae77f75c9af16ab45
Samuel Patrick <samuelpatrick97@gmail.com>
2018-10-20 22:02:23 +0800

    contact update

---
+++ php/sendContact.php
@@ -0,0 +1,41 @@
+<?php
+$servername = "localhost";
+$username = "f37ee";
+$password = "f37ee";
+$dbname = "f37ee";
+
+
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$name = $_POST["name"];
+
+
+$email = $_POST["email"];
+
+
+$subject = $_POST["subject"];
+
+$message =$_POST["message"];
+
+
+
+$sql_1 = "INSERT INTO `feedback` (`name`,`email`,`subject`,`message`) VALUES ('".$name."','".$email."','".$subject."','".$message."')";
+
+if (mysqli_query($conn, $sql_1)) {
+    $message = "Feedback has been received!";
+    echo "<script type='text/javascript'>alert('$message');
+    window.location.href='/sushi/contact.html';
+    </script>";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
+mysqli_close($conn);
+
+?>
---
+++ scripts/contact.js
@@ -0,0 +1,26 @@
+function checkName() {
+
+    var name = document.getElementById('name');
+    name.value = trim(name.value)
+        if (/^[A-Za-z][A-Za-z\s]*$/.test(name.value)==false)
+        {
+            alert('name invalid');
+            document.getElementById("name").value = "";
+            return false;
+        }
+    }
+    
+    function checkEmail() {
+    
+        var email = document.getElementById('email');
+        if (/^[\w.-]+@([\w]+\.){1,3}[\w]{2,3}$/.test(email.value)==false) 
+        {
+            alert('email invalid');
+            document.getElementById("email").value= "";
+            return false;
+        }
+    }
+    
+
+
+    
