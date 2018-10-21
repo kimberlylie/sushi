95b4f504e7eac44e343a403ff93776c227d29ea7
Samuel Patrick <samuelpatrick97@gmail.com>
2018-10-21 10:36:38 +0800

    menu

--- menu.php
+++ menu.php
@@ -42,18 +42,66 @@
                 </nav>
             </div>-->
 
+
+<?php
+$servername = "localhost";
+$username = "f37ee";
+$password = "f37ee";
+$dbname = "f37ee";
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+// quantity of iced cappuccino double
+$sql = "SELECT * FROM menu";
+$result = mysqli_query($conn, $sql);
+$items=array();
+$itemsName=array();
+$price = array();
+$image = array();
+if (mysqli_num_rows($result) > 0) {
+	// output data of each row
+	$i=0;
+	while($row = mysqli_fetch_assoc($result)) {
+        $itemsName[$i]=$row['name'];
+        $items[$i] =  str_replace(' ', '', $itemsName); 
+        $price[$i] = $row['price'];
+        $image[$i]= $row['imgURL'];
+		$i=$i+1;
+	}
+}
+
+
+
+if (!isset($_SESSION['cart']))
+{
+    $_SESSION['cart'] = array();
+    for ($i=0; $i<count($items); $i++)
+    {
+        $_SESSION['cart'][$i]=array($items[$i],0);
+    }
+}
+
+?>
+
+
             <div id="menu-list-container">
                 <ul>
-                    <li><img src="assets/menu/dummy.jpg" id="myItem"><p tag="Name">Name</p></li>
-                    <li><img src="assets/menu/dummy.jpg" id="myItem"><p tag="Name">Name</p></li>
-                    <li><img src="assets/menu/dummy.jpg" id="myItem"><p tag="Name">Name</p></li>
-                    <li><img src="assets/menu/dummy.jpg" id="myItem"><p tag="Name">Name</p></li>
-                    <li><img src="assets/menu/dummy.jpg" id="myItem"><p tag="Name">Name</p></li>
-                    <li><img src="assets/menu/dummy.jpg" id="myItem"><p tag="Name">Name</p></li>
+                <?php
+                for ($i=0; $i<count($items); $i++)
+                {
+                echo'<li><img src="'.$image[$i].'"><p>'.$itemsName[$i].'</p></li>';
+                }
+                ?>
                 </ul>
             </div>
 
-            <section>
+<!--             <section>
             <div class="footer">
                 <div id="icons">
                     <a href="#"><img src="assets/footer/fb_icon.jpg"></a>
@@ -83,7 +131,9 @@
                         </form>
                     </div>
                 </div>
-            </div>
+            </div> -->
+
+
 	    </div>
     </body>
 </html>
