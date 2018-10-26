08c13c8a0019a4c735b7fe1d9005ff83159c9d2c
Kimberly Lie <klie001@e.ntu.edu.sg>
2018-10-27 03:24:33 +0800

    update style

--- confirmEmail.php
+++ confirmEmail.php
@@ -14,10 +14,10 @@
 	    <div class="container">
             <div id="landing-contents">
                 <div id="landing-login">
-                    <img src="./assets/index/logo.png"><hr>
+                    <img src="./assets/login/logo.png"><hr>
                     <h3 style="color: gray; margin-bottom: 40px;">Please insert the 4-digit OTP we sent to your email.</h3>
                     <form action="checkOTP.php" method="post">
-                        <input type="text" class="otp" id="otp" maxLength="1" size="1" min="0" max="9" onchange="checkOTP()" style="margin-left: 228px; text-align: center;">
+                        <input type="text" class="otp" id="otp" maxLength="1" size="1" min="0" max="9" onchange="checkOTP()" style="margin-left: 320px; text-align: center;">
                         <input type="text" class="otp" id="otp" maxLength="1" size="1" min="0" max="9" onchange="checkOTP()" style="margin-left: 30px; text-align: center;">
                         <input type="text" class="otp" id="otp" maxLength="1" size="1" min="0" max="9" onchange="checkOTP()" style="margin-left: 30px; text-align: center;">
                         <input type="text" class="otp" id="otp" maxLength="1" size="1" min="0" max="9" onchange="checkOTP()" style="margin-left: 30px; text-align: center;">
@@ -26,7 +26,7 @@
                     </form> 
                     <a href="./signup.php" class="back-button" style="margin-top: 15px; width: 220px;">BACK TO SIGN UP</a>    
                 </div>
-                <div id="landing-image"><img src="./assets/index/cover1.jpg"></div>
+                <div id="landing-image"><img src="./assets/login/cover1f.jpg"></div>
             </div>
 	    </div>
     </body>
