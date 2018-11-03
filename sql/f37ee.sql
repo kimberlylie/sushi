3fa6ca8520f78e59195a77e2fd38e50997aa7dba
Samuel Patrick <samuelpatrick97@gmail.com>
2018-11-03 10:38:33 +0800

    no message

--- sql/f37ee.sql
+++ sql/f37ee.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 01, 2018 at 07:04 AM
+-- Generation Time: Nov 03, 2018 at 03:36 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -25,6 +25,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `customers`
 --
 
@@ -32,7 +51,7 @@ CREATE TABLE `customers` (
   `id` int(10) NOT NULL,
   `name` varchar(50) NOT NULL,
   `address` varchar(100) NOT NULL,
-  `postalCode` int(10) NOT NULL,
+  `postalCode` varchar(6) NOT NULL,
   `email` varchar(50) NOT NULL,
   `phone` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -42,18 +61,42 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`id`, `name`, `address`, `postalCode`, `email`, `phone`) VALUES
-(1, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423'),
-(2, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423'),
-(3, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423'),
-(4, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423'),
-(5, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423'),
-(6, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423'),
-(7, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423'),
-(8, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423'),
-(9, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423'),
-(10, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423'),
-(11, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423'),
-(12, 'sam ', '1234a4t adfjadf asdfda', 0, 'samuelparick97@gmail.com', '+6592383423');
+(1, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(2, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(3, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(4, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(5, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(6, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(7, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(8, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(9, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(10, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(11, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(12, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(13, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(14, 'sam ', '1234a4t adfjadf asdfda', '0', 'spatrick001@e.ntu.edu.sg', '+6592383423'),
+(15, 'sam asdf', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(16, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelpatrick97@gmail.com', '+6592383423'),
+(17, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelpatrick97@gmail.com', '+6592383423'),
+(18, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelparick97@gmail.com', '+6592383423'),
+(19, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelpatrick97@gmail.com', '+6592383423'),
+(20, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelpatrick97@gmail.com', '+6592383423'),
+(21, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelpatrick97@gmail.com', '+6592383423'),
+(22, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelpatrick97@gmail.com', '+6592383423'),
+(23, 'sam ', '1234a4t adfjadf asdfda', '0', 'samuelpatrick97@gmail.com', '+6592383423'),
+(24, 'sam ', '1234a4t adfjadf asdfda', '0', 'spatrick001@e.ntu.edu.sg', '+6592383423'),
+(25, 'robert', '1234a4t adfjadf asdfda', '000000', 'samuelparick@gmail.com', '+6592383423'),
+(26, 'sam ', '1234a4t adfjadf asdfda', '000000', 'samuelpatrick@gmail.com', '+6592383423'),
+(27, 'sam ', '1234a4t adfjadf asdfda', '000000', 'samuelparick97@gmail.com', '+6592383423'),
+(28, 'sam ', '1234a4t adfjadf asdfda', '000000', 'samuelpatrick@gmail.com', '+6592383423'),
+(29, 'sam ', '1234a4t adfjadf asdfda', '000000', 'test123@gmail.com', '+6592383423'),
+(30, 'kimberly ', '1234a4t adfjadf asdfda', '000000', 'klie001@e.ntu.edu.sg', '+6592383423'),
+(31, 'kiki', '1234a4t adfjadf asdfda', '000000', 'mrkiki@gmail.sg', '+6590000000'),
+(32, 'sam', '1234a4t adfjadf asdfda', '000000', 'samuelparick97@gmail.com', '+6592383423'),
+(33, 'delvin', '1234a4t adfjadf asdfda', '000000', 'delvin@gmail.com', '+6590001234'),
+(34, 'sam', '1234a4t adfjadf asdfda', '000000', 'samuelparick97@gmail.com', '+6592383423'),
+(35, 'sam', '1234a4t adfjadf asdfda', '000000', 'samuelparick97@gmail.com', '+6592383423'),
+(36, '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -68,6 +111,15 @@ CREATE TABLE `feedback` (
   `subject` varchar(80) NOT NULL,
   `message` varchar(140) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `email`, `subject`, `message`) VALUES
+(1, 'kimberly', 'klie@gmail.com', 'feedback', 'good'),
+(2, 'delvin', 'delvin@gmail.com', 'catering', '20 salmon nigiri for 24 november'),
+(3, 'rico', 'rico@gmail.com', 'others', 'when tuna nigiri will be available');
 
 -- --------------------------------------------------------
 
@@ -90,9 +142,21 @@ CREATE TABLE `feedback_member` (
 CREATE TABLE `member` (
   `id` int(10) NOT NULL,
   `customer_ID` int(10) NOT NULL,
-  `email` varchar(10) NOT NULL,
-  `password` varchar(64) NOT NULL
+  `email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id`, `customer_ID`, `email`, `password`) VALUES
+(11, 27, 'samuelparick97@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964'),
+(12, 28, 'samuelpatrick@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef'),
+(13, 29, 'test123@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964'),
+(14, 30, 'klie001@e.ntu.edu.sg', '40bd001563085fc35165329ea1ff5c5ecbdbbeef'),
+(15, 31, 'mrkiki@gmail.sg', '95752f86c99f1055feba64e03924cb71f0c08315'),
+(16, 33, 'delvin@gmail.com', '4aa1e7253d6c56353cd2a058ac4318f2ce7e5d6a');
 
 -- --------------------------------------------------------
 
@@ -115,11 +179,11 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`id`, `name`, `description`, `price`, `imgURL`, `type`, `availability`) VALUES
-(1, 'Crabstick Gunkan ', 'crab stick', 2, '/sushi/assets/menu/gunkan/crabstick.jpg', 'Gunkan', 1),
-(2, 'Salmon Gunkan ', 'Salmon', 2, '/sushi/assets/menu/gunkan/salmon.jpg', 'Gunkan', 1),
-(3, 'Shrimp Gunkan ', 'Shrimp', 2, '/sushi/assets/menu/gunkan/shrimp.jpg', 'Gunkan', 1),
+(1, 'Crabstick Gunkan ', '    crab stick', 2, '/sushi/assets/menu/gunkan/crabstick.jpg', 'Gunkan', 1),
+(2, 'Salmon Gunkan ', '  Salmon', 2, '/sushi/assets/menu/gunkan/salmon.jpg', 'Gunkan', 1),
+(3, 'Taiga Sushi Set', '   Salmon, tuna, crabstick, rice, cucumber, egg', 6, './assets/menu/sets/r4-768x512.jpg', 'Sets', 1),
 (4, 'Tuna Gunkan ', 'Tuna', 2, '/sushi/assets/menu/gunkan/tuna.jpg', 'Gunkan', 1),
-(5, 'Kyuri Sake Maguro', 'cucumber, Salmon, tuna, masago roe, tempura crisps, green onion, chilli sauce ', 1.5, '/sushi/assets/menu/maki/59_kyuri_sake_maguro-300x200.png', 'Maki', 1),
+(5, 'Kyuri Sake Maguro', '         cucumber, Salmon, tuna, masago roe, tempura crisps, green onion, chilli sauce ', 1.5, '/sushi/assets/menu/maki/59_kyuri_sake_maguro-300x200.png', 'Maki', 1),
 (6, 'Goma Masago', 'Crab sticks, masago roe, iceberg lettuce, cucumber, green onion, mayonnaise, sesame', 1.5, '/sushi/assets/menu/maki/63_goma_masago-300x200.png', 'Maki', 1),
 (7, 'Avocado Maki', 'avocado', 1.5, '/sushi/assets/menu/maki/Avocado-maki-300x200.jpg', 'Maki', 1),
 (8, 'Hitomaki', 'Salmon, avocado, cream', 1.5, '/sushi/assets/menu/maki/Hitomaki-380x250.png', 'Maki', 1),
@@ -133,7 +197,12 @@ INSERT INTO `menu` (`id`, `name`, `description`, `price`, `imgURL`, `type`, `ava
 (16, 'Philadelphia', 'Salmon, crab sticks, cucumber, cream', 1.5, '/sushi/assets/menu/maki/philadelphia-300x200.jpg', 'Maki', 1),
 (17, 'Salmon Nigiri', 'Salmon', 2.5, '/sushi/assets/menu/nigiri/nigiri-su-salmon-300x200.jpg', 'nigiri', 1),
 (18, 'Shrimp Nigiri', 'Shrimp', 2.5, '/sushi/assets/menu/nigiri/nigiri-su-shrimp-300x200.jpg', 'nigiri', 1),
-(19, 'Tuna Nigiri', 'Tuna', 2.5, '/sushi/assets/menu/nigiri/nigiri-su-tuna-300x200.jpg', 'nigiri', 1);
+(19, 'Tuna Nigiri', 'Tuna', 2.5, '/sushi/assets/menu/nigiri/nigiri-su-tuna-300x200.jpg', 'nigiri', 1),
+(20, 'Ryu Sushi Set', 'Salmon, tuna, cucumber, masago roe, sesame, eel', 8, './assets/menu/sets/r5-sotusis-rinkinys-768x512.jpg', 'Sets', 1),
+(21, 'Zen Sushi Set', 'Crabstick, cucumber, salmon, cream', 6, './assets/menu/sets/r6-768x512.jpg', 'Sets', 1),
+(22, 'Yasai Bowl', 'Grilled tofu, rice, avocado', 3, './assets/menu/don/01-380x250.jpg', 'Don', 1),
+(23, 'Teka Bowl', 'Tuna, rice, wakame seaweed', 3, './assets/menu/don/04-380x250.jpg', 'Don', 1),
+(24, 'Ebi Bowl', 'Tiger prawns, rice', 3, './assets/menu/don/03-380x250.jpg', 'Don', 1);
 
 -- --------------------------------------------------------
 
@@ -154,7 +223,26 @@ CREATE TABLE `orders` (
 
 INSERT INTO `orders` (`id`, `transaction_ID`, `menu_ID`, `quantity`) VALUES
 (9, 3, 6, 3),
-(10, 3, 10, 4);
+(10, 3, 10, 4),
+(11, 4, 6, 5),
+(12, 4, 7, 5),
+(13, 5, 6, 4),
+(14, 5, 14, 5),
+(15, 6, 6, 2),
+(16, 6, 10, 3),
+(17, 6, 2, 2),
+(18, 7, 6, 5),
+(19, 7, 10, 7),
+(20, 8, 6, 3),
+(21, 8, 10, 4),
+(22, 9, 6, 4),
+(23, 9, 7, 5),
+(24, 10, 7, 5),
+(25, 10, 9, 1),
+(26, 11, 23, 4),
+(27, 12, 6, 1),
+(28, 13, 16, 4),
+(29, 13, 7, 4);
 
 -- --------------------------------------------------------
 
@@ -168,21 +256,38 @@ CREATE TABLE `transaction` (
   `date` date NOT NULL,
   `ship_address` varchar(100) NOT NULL,
   `ship_postalCode` int(6) NOT NULL,
-  `note` varchar(140) NOT NULL
+  `note` varchar(140) NOT NULL,
+  `price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `transaction`
 --
 
-INSERT INTO `transaction` (`id`, `customer_ID`, `date`, `ship_address`, `ship_postalCode`, `note`) VALUES
-(1, 10, '2018-11-01', '1234a4t adfjadf asdfda', 0, ''),
-(2, 11, '2018-11-01', '1234a4t adfjadf asdfda', 0, ''),
-(3, 12, '2018-11-01', '1234a4t adfjadf asdfda', 0, '');
+INSERT INTO `transaction` (`id`, `customer_ID`, `date`, `ship_address`, `ship_postalCode`, `note`, `price`) VALUES
+(1, 10, '2018-11-01', '1234a4t adfjadf asdfda', 0, '', 0),
+(2, 11, '2018-11-01', '1234a4t adfjadf asdfda', 0, '', 0),
+(3, 12, '2018-11-01', '1234a4t adfjadf asdfda', 0, '', 0),
+(4, 13, '2018-11-01', '1234a4t adfjadf asdfda', 0, '', 15),
+(5, 14, '2018-11-01', '1234a4t adfjadf asdfda', 0, '', 18.445),
+(6, 15, '2018-11-01', '1234a4t adfjadf asdfda', 0, '', 16.305),
+(7, 31, '2018-11-01', '1234a4t adfjadf', 0, '', 23.26),
+(8, 32, '2018-11-02', '1234a4t adfjadf asdfda', 0, '', 15.235),
+(9, 31, '2018-11-02', '1234a4t adfjadf asdfda', 0, '', 18.445),
+(10, 31, '2018-11-02', '1234a4t adfjadf asdfda', 0, '', 13.63),
+(11, 33, '2018-11-02', '1234a4t adfjadf asdfda', 0, '', 16.84),
+(12, 34, '2018-11-02', '1234a4t adfjadf asdfda', 0, 'afdsf', 5.605),
+(13, 35, '2018-11-02', '1234a4t adfjadf asdfda', 0, '', 16.84);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `customers`
@@ -209,7 +314,8 @@ ALTER TABLE `feedback_member`
 --
 ALTER TABLE `member`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `customer_ID` (`customer_ID`) USING BTREE;
+  ADD UNIQUE KEY `customer_ID` (`customer_ID`) USING BTREE,
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `menu`
@@ -237,16 +343,22 @@ ALTER TABLE `transaction`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `feedback_member`
@@ -258,25 +370,25 @@ ALTER TABLE `feedback_member`
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
